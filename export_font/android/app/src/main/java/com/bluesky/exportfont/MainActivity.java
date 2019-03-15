e9b5e8209e785ad927f809e4e30e1dd6567ef90a
Thao Nguyen <thaonh@sigma-solutions.eu>
2019-03-15 16:43:32 +0700

    Exporting fonts

---
+++ export_font/android/app/src/main/java/com/bluesky/exportfont/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bluesky.exportfont;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
